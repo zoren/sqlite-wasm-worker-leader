5272840d7fde222f2701681ee0a853eac56f7955
Søren Sjørup <soren@zoren.dk>
2023-11-09 20:04:51 +0100

    let's try explicit path

--- src/node/nodeWorker.js
+++ src/node/nodeWorker.js
@@ -1,8 +1,5 @@
 import { parentPort } from 'node:worker_threads'
-// this import only works because we have added to 
-// node_modules/@sqlite.org/sqlite-wasm/package.json export section:
-// "./jswasm/sqlite3-node.mjs": "./sqlite-wasm/jswasm/sqlite3-node.mjs"
-import sqlite3InitModuleNode from '@sqlite.org/sqlite-wasm/jswasm/sqlite3-node.mjs'
+import sqlite3InitModuleNode from '../../node_modules/@sqlite.org/sqlite-wasm/sqlite-wasm/jswasm/sqlite3-node.mjs'
 import { init } from '../worker.js'
 
 init(
